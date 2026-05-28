7e378cf0ea112321bf91bcd6d24aebd2a7676837
kornnnnnne <k21855271@gmail.com>
2026-05-28 22:38:25 +0900

    Update script.js

--- script.js
+++ script.js
@@ -4,13 +4,3 @@ const message = document.getElementById("message");
 button.addEventListener("click", () => {
   message.textContent = "ゲームスタート！！！！！";
 });
-
-const playerName = prompt("名前を入力");
-
-function saveScore(score) {
-  localStorage.setItem(playerName + "_score", score);
-}
-
-function loadScore() {
-  return localStorage.getItem(playerName + "_score");
-}
